11cf70e6bf31b18eafceb0d06d75c86e0c17b218
jaclu <jacob.lundqvist@gmail.com>
2024-08-17 21:35:34 +0200

    settings_local in gitignore

--- brabantse_gebouwen/settings_local.py
+++
@@ -1,9 +0,0 @@
-try:
-    from .brabantse_gebouwen.settings import *
-except ImportError:
-    pass
-
-
-ELASTICSEARCH_HOSTS = [{"scheme": "http", "host": "localhost", "port": 9200}]
-GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
-GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
